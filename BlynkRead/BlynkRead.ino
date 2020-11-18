2ccca2615b545a7c99becc494e4ede05ea401c0f
Justin Caro <justincaro1130@gmail.com>
2020-11-18 12:19:41 -0700

    Add files via upload
    This code uses the Blynk code examples that are provided for a data push and joystick as a base.

---
+++ BlynkRead/BlynkRead.ino
@@ -0,0 +1,111 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+  WARNING!
+    It's very tricky to get it working. Please read this article:
+    http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
+
+  This example shows how to send values to the Blynk App,
+  when there is a widget, attached to the Virtual Pin and it
+  is set to some frequency
+
+  Project setup in the app:
+    Value Display widget attached to V5. Set any reading
+    frequency (i.e. 1 second)
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+int datain;
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "YourAuthToken";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "YourNetworkName";
+char pass[] = "YourPassword";
+
+// or Software Serial on Uno, Nano...
+#include <SoftwareSerial.h>
+SoftwareSerial EspSerial(2, 3); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+// Use Virtual pin 5 for uptime display
+#define SENSOR V5
+
+
+// This function tells Arduino what to do if there is a Widget
+// which is requesting data for Virtual Pin (5)
+BLYNK_READ(SENSOR)
+{
+  // This command writes Arduino's uptime in seconds to Virtual Pin (5)
+  Blynk.virtualWrite(SENSOR, datain);
+}
+
+BLYNK_WRITE(V1) {
+  int x = param[0].asInt();
+  int y = param[1].asInt();
+
+  // Do something with x and y
+  Serial.print("X = ");
+  Serial.print(x);
+  Serial.print("; Y = ");
+  Serial.println(y);
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  Blynk.begin(auth, wifi, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+
+  if (Serial.available())
+  {
+    int datain = Serial.read();
+    Serial.print(" Sensor = ");
+    Serial.print(datain);
+  }
+  /* Joystick values are from 0 to 255
+   *  Stationary Position 127 127
+   *  x goes to zero on the left side y zero on buttom
+   *  Forward and right means largest number and smallest values are backwars and left
+   *  Without knowing the full capabilities of the servos and driver I dont know how to limit their movements
+   */
+}
